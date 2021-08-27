90b9bbdcf56213c64969eb5fc3765e74f11b44ee
mohamed006 <mohabziz@outlook.com>
2021-08-27 07:26:54 -0400

    add job views and url

--- job/admin.py
+++ job/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Job
+from .models import Job , Category
+
 admin.site.register(Job)
+
+admin.site.register(Category)
 
 # Register your models here.
--- job/views.py
+++ job/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def job_list (request):
+    pass
+
+def job_detail (request , id):
+    pass
+
 # Create your views here.
